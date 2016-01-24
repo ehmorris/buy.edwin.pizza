cbf380327502f597722ed33f0b2963af002cb037
Edwin <edwin.h.morris@gmail.com>
2016-01-23 22:24:10 -0500

    implement sticky scroll with stickem
    this isn't working. believe it's because it depends on an older version
    of jQuery

--- app/assets/javascripts/_waypoints.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.famer-video').each(function() {
-    new Waypoint.Sticky({
-      element: this,
-      wrapper: '<div class="famer-video_wrapper" />',
-      stuckClass: 'famer-video--docked'
-    });
-  });
-});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,5 @@
 //= require jquery_ujs
 //= require _fastclick-1.0.6
 //= require _fitvids-1.1.0
-//= require _waypoints-4.0.0
-//= require _sticky-4.0.0
+//= require _stickem-1.4.1
 //= require_tree .
