69965142dffd736fbc911203b58b5082516d6204
Edwin Morris <edwin@rapgenius.com>
2017-03-06 11:05:26 -0500

    remove and rename a bunch of libraries. WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,6 @@
 //= require jquery_ujs
 //= require _fastclick-1.0.6
 //= require _fitvids-1.1.0
-//= require _waypoints-4.0.0
-//= require _sticky-4.0.0
+//= require _noframework.waypoints-4.0.1
+//= require _inview-4.0.1
 //= require_tree .
