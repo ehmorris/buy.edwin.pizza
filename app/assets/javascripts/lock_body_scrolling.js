3b365873d8cf924289fabac690961b789d17c122
Edwin Morris <edwin@rapgenius.com>
2017-03-18 16:43:47 -0400

    update js, load js non-blocking

--- app/assets/javascripts/lock_body_scrolling.js
+++ app/assets/javascripts/lock_body_scrolling.js
@@ -1,16 +1,16 @@
 $(function() {
-  var prevent_touchmove = function(e) {
+  let prevent_touchmove = (e) => {
     e.preventDefault();
   };
 
-  $('[lock-body-scrolling]').on('click', function() {
+  $('[lock-body-scrolling]').on('click', () => {
     $(document).scrollTop(0);
 
     $('body').addClass('lock_body_scrolling');
     $(document).on('touchmove', prevent_touchmove);
   });
 
-  $('[unlock-body-scrolling]').on('click', function() {
+  $('[unlock-body-scrolling]').on('click', () => {
     $('body').removeClass('lock_body_scrolling');
     $(document).off('touchmove', prevent_touchmove);
   });
--- app/assets/javascripts/slides.js
+++ app/assets/javascripts/slides.js
@@ -1,10 +1,10 @@
 $(function() {
-  var hidden_class = 'slides-slide-hidden';
+  let hidden_class = 'slides-slide-hidden';
 
   $('[slide]:not(:first-child)').addClass(hidden_class);
 
-  $('[open-next-slide]').on('click', function() {
-    var $this_slide = $(this).parents('[slide]');
+  $('[open-next-slide]').on('click', (e) => {
+    let $this_slide = $(e.currentTarget).parents('[slide]');
 
     $this_slide.next('[slide]').removeClass(hidden_class);
     $this_slide.addClass(hidden_class);
