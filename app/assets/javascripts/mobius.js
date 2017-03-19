cb76ebef99b87ab087819f12ff269ac7815e737d
Edwin Morris <edwin@rapgenius.com>
2017-03-19 17:41:55 -0400

    mobius can be triggered from top or bottom of page

--- app/assets/javascripts/mobius.js
+++ app/assets/javascripts/mobius.js
@@ -1,11 +1,41 @@
 $(function() {
-  new Waypoint.Inview({
-    element: $('#mobius_trigger')[0],
-    enter: function() {
-      $(document).scrollTop(0);
-      $('.success_message').toggleClass('success_message--inverted');
-      $('.scroll').toggleClass('scroll--inverted');
-    }
-  });
+  let document_height = $(document).height();
+  let window_height = $(window).height();
+  let trigger_height = window_height;
+
+  let invert_page = () => {
+    $('.success_message').toggleClass('success_message--inverted');
+    $('.scroll').toggleClass('scroll--inverted');
+  };
+
+  let set_mobius_to_top = () => {
+    $(document).scrollTop(trigger_height);
+    invert_page();
+  };
+
+  let set_mobius_to_bottom = () => {
+    $(document).scrollTop(document_height - window_height - trigger_height);
+    invert_page();
+  };
+
+  let initialize_mobius = () => {
+    let bottom_trigger = new Waypoint.Inview({
+      element: $('#mobius_bottom_trigger')[0],
+      enter: set_mobius_to_top
+    });
+
+    let top_trigger = new Waypoint.Inview({
+      element: $('#mobius_top_trigger')[0],
+      enter: set_mobius_to_bottom
+    });
+  };
+
+  let initialize_page = () => {
+    $(document).scrollTop(window_height + trigger_height);
+  }
+
+  initialize_page();
+
+  window.setTimeout(initialize_mobius, 1000);
 });
 
