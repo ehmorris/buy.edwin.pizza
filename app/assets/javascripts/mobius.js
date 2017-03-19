df44406f9393fab2a435832a76fae9ccc7236366
Edwin Morris <edwin@rapgenius.com>
2017-03-19 18:09:58 -0400

    compensate for safari UI height
    this way we don't accidentally trigger things one after another

--- app/assets/javascripts/mobius.js
+++ app/assets/javascripts/mobius.js
@@ -2,6 +2,7 @@ $(function() {
   let document_height = $(document).height();
   let window_height = $(window).height();
   let trigger_height = window_height;
+  let safari_ui_height = 108;
 
   let invert_page = () => {
     $('.success_message').toggleClass('success_message--inverted');
@@ -9,12 +10,12 @@ $(function() {
   };
 
   let set_mobius_to_top = () => {
-    $(document).scrollTop(trigger_height);
+    $(document).scrollTop(trigger_height + safari_ui_height);
     invert_page();
   };
 
   let set_mobius_to_bottom = () => {
-    $(document).scrollTop(document_height - window_height - trigger_height);
+    $(document).scrollTop(document_height - window_height - trigger_height - safari_ui_height);
     invert_page();
   };
 
