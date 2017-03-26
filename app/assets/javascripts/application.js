7c9b853f75f9e5684a6377a1eaccf7182615dfee
Edwin Morris <edwin@rapgenius.com>
2017-03-25 22:57:50 -0400

    rename es6 files, convert remaining to es5
    correctly use babel via
    http://nandovieira.com/using-es2015-with-asset-pipeline-on-ruby-on-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
+//= require babel
 //= require jquery
 //= require jquery_ujs
 //= require _noframework.waypoints-4.0.1
 //= require_tree ../../../vendor/assets/javascripts
 //= require_tree .
+//= require_self
