2b9eb5567cc179e554d4bf0c3a070ed0166848a6
Edwin Morris <edwin.h.morris@gmail.com>
2019-09-14 13:38:22 -0400

    Update all entries to show their video inline

--- app/helpers/s3_image_tag_helper.rb
+++ app/helpers/s3_image_tag_helper.rb
@@ -1,5 +1,5 @@
 module S3ImageTagHelper
   def s3_image_tag(filename)
-    raw "<img src=\"https://s3.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/#{filename}\" />"
+    raw "<img loading='lazy' src=\"https://s3.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/#{filename}\" />"
   end
 end
